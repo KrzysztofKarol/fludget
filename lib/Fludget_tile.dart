1192ddfbf39df15f53db77f8cddc2c2b177360e9
AjayKumar <ajay88491@gmial.com>
2019-06-01 14:11:15 +0200

    Modified Tiles

---
+++ lib/Fludget_tile.dart
@@ -0,0 +1,89 @@
+import 'dart:math';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FludgetTile extends StatefulWidget {
+  @override
+  _FludgetTileState createState() => _FludgetTileState();
+}
+
+class _FludgetTileState extends State<FludgetTile> {
+  String _imageUrl;
+  StorageReference _reference = FirebaseStorage.instance.ref().child('');
+  void initState() {
+    super.initState();
+  }
+
+  Future getImage() async {
+    String downloadUrl = await _reference.getDownloadURL();
+    setState(() {
+      _imageUrl = downloadUrl;
+    });
+  }
+
+  var colors = [0xff03a9f4, 0xff4db6ac, 0xfff44336, 0xffe91e63, 0xff7c4dff];
+  var random = Random();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+//        height: 100.0,
+        child: Card(
+      color: Color(colors[random.nextInt(colors.length - 1)]),
+      elevation: 8.0,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(20.0))),
+      child: Container(
+        height: 150,
+        child: Column(
+          children: <Widget>[
+            Flexible(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Widget of the Day",
+                  style: TextStyle(fontSize: 30.0, color: Colors.white),
+                ),
+              ),
+            ),
+            Flexible(
+              flex: 2,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Flexible(
+                    child: Container(
+                      color: Colors.white,
+                      width: 100.0,
+                      height: 70.0,
+                    ),
+                    flex: 4,
+                  ),
+                  Flexible(
+                    flex: 4,
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text(
+                          "Align",
+                          style: TextStyle(
+                              color: Colors.white,
+                              letterSpacing: 1.0,
+                              fontWeight: FontWeight.w400,
+                              fontSize: 25.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}
--- lib/fludget_home_page.dart
+++ lib/fludget_home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fludget/Fludget_tile.dart';
 
 class FludgetHomePage extends StatefulWidget {
   FludgetHomePage({Key key}) : super(key: key);
@@ -11,25 +12,25 @@ class _FludgetHomePageState extends State<FludgetHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          ListTile(
-            title: Text('Widget of the day'),
-          ),
-          ListTile(
-            title: Text('Package of the day'),
-          ),
-          ListTile(
-            title: Text('Github repo of the day'),
-          ),
-          ListTile(
-            title: Text('Flutter app of the day'),
-          ),
-        ],
-      ),
-    ));
+        body: Center(child: FludgetTile()
+//      Column(
+//        crossAxisAlignment: CrossAxisAlignment.center,
+//        mainAxisAlignment: MainAxisAlignment.center,
+//        children: <Widget>[
+//          ListTile(
+//            title: Text('Widget of the day'),
+//          ),
+//          ListTile(
+//            title: Text('Package of the day'),
+//          ),
+//          ListTile(
+//            title: Text('Github repo of the day'),
+//          ),
+//          ListTile(
+//            title: Text('Flutter app of the day'),
+//          ),
+//        ],
+//      ),
+            ));
   }
 }
