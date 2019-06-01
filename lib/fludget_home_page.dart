23fb2e8cd664a8a9f1430307804582e65c20f8c0
shvchuk <andrzej.szewczuk@gmail.com>
2019-06-01 12:35:14 +0200

    webview

--- lib/fludget_home_page.dart
+++ lib/fludget_home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class FludgetHomePage extends StatefulWidget {
   FludgetHomePage({Key key}) : super(key: key);
@@ -24,6 +25,16 @@ class _FludgetHomePageState extends State<FludgetHomePage> {
 
               return ListTile(
                 title: Text(snapshot.data.documents[0].data["name"]),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => WidgetOfTheDayPage(
+                                url: snapshot.data.documents[0].data["url"],
+                                widgetName:
+                                    snapshot.data.documents[0].data["name"],
+                              )));
+                },
               );
             },
           ),
@@ -39,5 +50,33 @@ class _FludgetHomePageState extends State<FludgetHomePage> {
         ],
       ),
     ));
+  }
+}
+
+class WidgetOfTheDayPage extends StatelessWidget {
+  final String url;
+  final String widgetName;
+  const WidgetOfTheDayPage({
+    this.url,
+    this.widgetName,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Widget of the day: $widgetName'),
+        leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+            }),
+      ),
+      body: WebView(
+        key: UniqueKey(),
+        javascriptMode: JavascriptMode.unrestricted,
+        initialUrl: url,
+      ),
+    );
   }
 }
