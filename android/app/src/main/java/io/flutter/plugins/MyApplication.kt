3dcc9ed6eb8d065d27d72b22085e04df22302b2b
AjayKumar <ajay88491@gmial.com>
2019-06-01 14:24:51 +0200

    New card

---
+++ android/app/src/main/java/io/flutter/plugins/MyApplication.kt
@@ -0,0 +1,13 @@
+package io.flutter.plugins
+
+import android.content.Context
+import androidx.multidex.MultiDex
+import io.flutter.app.FlutterApplication
+
+class MyApplication : FlutterApplication() {
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
+}
--- lib/fludget_home_page.dart
+++ lib/fludget_home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:fludget/Fludget_tile.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class FludgetHomePage extends StatefulWidget {
   FludgetHomePage({Key key}) : super(key: key);
@@ -12,25 +13,70 @@ class _FludgetHomePageState extends State<FludgetHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(child: FludgetTile()
-//      Column(
-//        crossAxisAlignment: CrossAxisAlignment.center,
-//        mainAxisAlignment: MainAxisAlignment.center,
-//        children: <Widget>[
-//          ListTile(
-//            title: Text('Widget of the day'),
-//          ),
-//          ListTile(
-//            title: Text('Package of the day'),
-//          ),
-//          ListTile(
-//            title: Text('Github repo of the day'),
-//          ),
-//          ListTile(
-//            title: Text('Flutter app of the day'),
-//          ),
-//        ],
-//      ),
-            ));
+        body: Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          StreamBuilder<QuerySnapshot>(
+            stream: Firestore.instance.collection('widgets').snapshots(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) return LinearProgressIndicator();
+
+              return ListTile(
+                title: Text(snapshot.data.documents[0].data["name"]),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => WidgetOfTheDayPage(
+                                url: snapshot.data.documents[0].data["url"],
+                                widgetName:
+                                    snapshot.data.documents[0].data["name"],
+                              )));
+                },
+              );
+            },
+          ),
+          ListTile(
+            title: Text('Package of the day'),
+          ),
+          ListTile(
+            title: Text('Github repo of the day'),
+          ),
+          ListTile(
+            title: Text('Flutter app of the day'),
+          ),
+        ],
+      ),
+    ));
+  }
+}
+
+class WidgetOfTheDayPage extends StatelessWidget {
+  final String url;
+  final String widgetName;
+  const WidgetOfTheDayPage({
+    this.url,
+    this.widgetName,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Widget of the day: $widgetName'),
+        leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+            }),
+      ),
+      body: WebView(
+        key: UniqueKey(),
+        javascriptMode: JavascriptMode.unrestricted,
+        initialUrl: url,
+      ),
+    );
   }
 }
